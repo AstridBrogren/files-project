de1eeec049e470070ff2d771a53af91da581cb94
AstridBrogren <astrid_brogren@biswashington.org>
2025-01-23 17:01:05 +0000

    done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+with open("new_file.txt", "a") as file:
+    file.write("text.\n")
 
+with open("new_file.txt", "r") as file:
+    for line in file:
+        print(line[1])
+
+with open("new_file.txt", "r") as file:
+    for line in file:
+        if len(line) > 7:
+            print(line.strip())
+
+new_line = input("24")
+with open("new_file.txt", "a") as file:
+    file.write(new_line + "\n")
